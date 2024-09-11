f48ab405706921cbbad95170726cd7a5913add57
xThezs <rudy.clot.pro@gmail.com>
2024-09-11 15:20:45 +0200

    add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { UploadComponent } from './components/upload/upload.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent,SignupComponent,UploadComponent,HomeComponent],
+  imports: [RouterOutlet,LoginComponent,SignupComponent,UploadComponent,HomeComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
